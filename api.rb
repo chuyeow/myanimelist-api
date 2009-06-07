67c14dbcc1fdcde19ff370d8c8ce67936de2054f
Cheah Chu Yeow <chuyeow@gmail.com>
2009-06-07 23:02:38 +0800

    Make a note about using my (chuyeow's) fork of Curb for cookiefile support, and use this option to re-use the authentication cookies in the HTTP POST in '/anime'.

--- api.rb
+++ api.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'curb'
+require 'tempfile'
 
 curl = Curl::Easy.new
 curl.enable_cookies = true
@@ -10,9 +11,11 @@ use Rack::Auth::Basic do |username, password|
   # Authenticate with MyAnimeList.net.
   # FIXME We should store our own cookie with the user so that auth is not done unnecessarily for all requests.
   curl.url = 'http://myanimelist.net/login.php'
+  tempfile = Tempfile.new('cookies', 'tmp')
+  curl.cookiejar = tempfile.path
+
   authenticated = false
   curl.on_header { |header|
-puts header
     # A HTTP 302 redirection to the MAL panel indicates successful authentication.
     authenticated = true if header =~ %r{^Location: http://myanimelist.net/panel.php\s+}
 
@@ -20,8 +23,14 @@ puts header
   }
   curl.http_post(
     Curl::PostField.content('username', username),
-    Curl::PostField.content('password', password)
+    Curl::PostField.content('password', password),
+    Curl::PostField.content('cookies', '1')
   )
+
+  # Set the cookiefile for subsequent Curl requests and disable the cookiejar.
+  curl.cookiefile = tempfile.path
+  curl.cookiejar = nil
+  tempfile.close
 
   # Reset the on_header handler.
   curl.on_header
