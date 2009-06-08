395f05e0f07b3998ac14337191969b53ffb66da9
Cheah Chu Yeow <chuyeow@gmail.com>
2009-06-08 19:51:39 +0800

    Use simple cookie string instead (requires patched Curb). Still a work in progress.

--- api.rb
+++ api.rb
@@ -1,21 +1,33 @@
 require 'rubygems'
 require 'sinatra'
 require 'curb'
-require 'tempfile'
+
+# Sinatra settings.
+set :app_file, __FILE__
+set :reload, true
+
 
 curl = Curl::Easy.new
 curl.enable_cookies = true
 
+cookies = []
+
 use Rack::Auth::Basic do |username, password|
+
+  return true if cookies.length > 0
 
   # Authenticate with MyAnimeList.net.
   # FIXME We should store our own cookie with the user so that auth is not done unnecessarily for all requests.
   curl.url = 'http://myanimelist.net/login.php'
-  tempfile = Tempfile.new('cookies', 'tmp')
-  curl.cookiejar = tempfile.path
 
   authenticated = false
   curl.on_header { |header|
+
+    # Parse cookies from the headers (yes, this is a naive implementation but it's fast).
+    if header =~ /^Set-Cookie: ([^=])=([^;]+;)/
+      cookies << "#{$1}=#{$2}"
+    end
+
     # A HTTP 302 redirection to the MAL panel indicates successful authentication.
     authenticated = true if header =~ %r{^Location: http://myanimelist.net/panel.php\s+}
 
@@ -27,11 +39,6 @@ use Rack::Auth::Basic do |username, password|
     Curl::PostField.content('cookies', '1')
   )
 
-  # Set the cookiefile for subsequent Curl requests and disable the cookiejar.
-  curl.cookiefile = tempfile.path
-  curl.cookiejar = nil
-  tempfile.close
-
   # Reset the on_header handler.
   curl.on_header
 
@@ -41,11 +48,13 @@ end
 
 get '/anime' do
 
+  curl.cookies = cookies.join(' ')
+
   curl.url = 'http://myanimelist.net/panel.php?go=export'
   curl.http_post(
     Curl::PostField.content('type', '1'),
     Curl::PostField.content('subexport', 'Export My List')
   )
-
+  
   curl.body_str
 end
