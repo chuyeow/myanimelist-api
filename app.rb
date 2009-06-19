ec52cfeb15f1959846cae76f5541dcb5a98d4829
Cheah Chu Yeow <chuyeow@gmail.com>
2009-06-19 15:19:27 +0800

    Move MyAnimeList::Auth into a separate file, and move Anime class into MyAnimeList module.

--- app.rb
+++ app.rb
@@ -4,72 +4,10 @@ require 'curb'
 require 'nokogiri'
 require 'json'
 
-require 'anime'
+require 'my_anime_list/anime'
 
 # Sinatra settings.
 set :sessions, true
-
-module MyAnimeList
-  module Auth
-
-    def auth
-      @auth ||= Rack::Auth::Basic::Request.new(request.env)
-    end
-
-    def unauthenticated!(realm = 'myanimelist.net')
-      headers['WWW-Authenticate'] = %(Basic realm="#{realm}")
-      throw :halt, [ 401, 'Authorization Required' ]
-    end
-
-    def bad_request!
-      throw :halt, [ 400, 'Bad Request' ]
-    end
-
-    def authenticated?
-      request.env['REMOTE_USER']
-    end
-
-    # Authenticate with MyAnimeList.net.
-    def authenticate_with_mal(username, password)
-
-      curl = Curl::Easy.new('http://myanimelist.net/login.php')
-
-      authenticated = false
-      cookies = []
-      curl.on_header { |header|
-
-        # Parse cookies from the headers (yes, this is a naive implementation but it's fast).
-        cookies << "#{$1}=#{$2}" if header =~ /^Set-Cookie: ([^=])=([^;]+;)/
-
-        # A HTTP 302 redirection to the MAL panel indicates successful authentication.
-        authenticated = true if header =~ %r{^Location: http://myanimelist.net/panel.php\s+}
-
-        header.length
-      }
-      curl.http_post(
-        Curl::PostField.content('username', username),
-        Curl::PostField.content('password', password),
-        Curl::PostField.content('cookies', '1')
-      )
-
-      # Reset the on_header handler.
-      curl.on_header
-
-      # Save cookie string into session.
-      session['cookie_string'] = cookies.join(' ') if authenticated
-
-      authenticated
-    end
-
-    def authenticate
-      return if authenticated?
-      unauthenticated! unless auth.provided?
-      bad_request! unless auth.basic?
-      unauthenticated! unless authenticate_with_mal(*auth.credentials)
-      request.env['REMOTE_USER'] = auth.username
-    end
-  end
-end
 
 helpers do
   include MyAnimeList::Auth
@@ -89,7 +27,7 @@ get '/anime/:id' do
   content_type :json
 
   # FIXME This should catch any exceptions, such as 404 or network/HTTP errors.
-  anime = Anime.scrape_anime(params[:id], session['cookie_string'])
+  anime = MyAnimeList::Anime.scrape_anime(params[:id], session['cookie_string'])
 
   anime.to_json
 end
@@ -122,7 +60,7 @@ get '/anime' do
   xml_doc = Nokogiri::XML.parse(response)
 
   anime_list = xml_doc.search('anime').map do |anime_node|
-    anime = Anime.new
+    anime = MyAnimeList::Anime.new
     anime.id                = anime_node.at('series_animedb_id').text
     anime.title             = anime_node.at('series_title').text
     anime.type              = anime_node.at('series_type').text
