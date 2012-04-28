05e53e47f31d232f68f7e64cc1d09ac672603ee3
Cheah Chu Yeow <chuyeow@gmail.com>
2012-04-28 14:43:32 +0800

    Point to production server mal-api.com.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,13 @@
 set :application, 'myanimelist-api'
-set :repository,  "git://github.com/chuyeow/#{application}.git"
-set :deploy_to, "/var/rackapps/#{application}"
+set :repository,  'git://github.com/chuyeow/myanimelist-api.git'
+set :deploy_to, '/var/rackapps/myanimelist-api'
 set :scm, :git
 set :deploy_via, :remote_cache
+
+set :user, 'deploy'
+set :runner, 'deploy'
 set :use_sudo, false
 
 set :normalize_asset_timestamps, false
 
-server '175.41.136.29', :app, :web, :db
+server 'mal-api.com', :app, :web, :db
